a2bab939b3a5babb99daec69d2ecb24b34cdb5d3
Giorgio <barnabogiorgio@gmail.com>
2021-05-14 11:30:08 +0200

    wandb

--- train.py
+++ train.py
@@ -12,46 +12,46 @@ from hydra.utils import get_original_cwd
 from omegaconf import DictConfig, OmegaConf
 
 import logging
+
 logger = logging.getLogger(__name__)
+
+import wandb
+
+# Set up your default hyperparameters
+hparams_default = dict(base_filters=16, lr=10 ^ -4, sample_rate=4000)
+
 
 class ESC50Dataset(torch.utils.data.Dataset):
     # Simple class to load the desired folders inside ESC-50
-    
-    def __init__(self, path: Path = Path('data/ESC-50'), 
-                 sample_rate: int = 8000,
-                 folds = [1]):
+
+    def __init__(
+        self, path: Path = Path("data/ESC-50"), sample_rate: int = 8000, folds=[1]
+    ):
         # Load CSV & initialize all torchaudio.transforms:
         # Resample --> MelSpectrogram --> AmplitudeToDB
         self.path = path
-        self.csv = pd.read_csv(path / Path('meta/esc50.csv'))
-        self.csv = self.csv[self.csv['fold'].isin(folds)]
+        self.csv = pd.read_csv(path / Path("meta/esc50.csv"))
+        self.csv = self.csv[self.csv["fold"].isin(folds)]
         self.resample = torchaudio.transforms.Resample(
             orig_freq=44100, new_freq=sample_rate
         )
-        self.melspec = torchaudio.transforms.MelSpectrogram(
-            sample_rate=sample_rate)
+        self.melspec = torchaudio.transforms.MelSpectrogram(sample_rate=sample_rate)
         self.db = torchaudio.transforms.AmplitudeToDB(top_db=80)
-        
-        
+
     def __getitem__(self, index):
         # Returns (xb, yb) pair, after applying all transformations on the audio file.
         row = self.csv.iloc[index]
-        wav, _ = torchaudio.load(self.path / 'audio' / row['filename'])
-        label = row['target']
-        xb = self.db(
-            self.melspec(
-                self.resample(wav)
-            )
-        )
+        wav, _ = torchaudio.load(self.path / "audio" / row["filename"])
+        label = row["target"]
+        xb = self.db(self.melspec(self.resample(wav)))
         return xb, label
-        
+
     def __len__(self):
         # Returns length
         return len(self.csv)
 
 
 class AudioNet(pl.LightningModule):
-    
     def __init__(self, hparams: DictConfig):
         super().__init__()
         print(hparams)
@@ -61,13 +61,17 @@ class AudioNet(pl.LightningModule):
         self.conv2 = nn.Conv2d(hparams.base_filters, hparams.base_filters, 3, padding=1)
         self.bn2 = nn.BatchNorm2d(hparams.base_filters)
         self.pool1 = nn.MaxPool2d(2)
-        self.conv3 = nn.Conv2d(hparams.base_filters, hparams.base_filters * 2, 3, padding=1)
+        self.conv3 = nn.Conv2d(
+            hparams.base_filters, hparams.base_filters * 2, 3, padding=1
+        )
         self.bn3 = nn.BatchNorm2d(hparams.base_filters * 2)
-        self.conv4 = nn.Conv2d(hparams.base_filters * 2, hparams.base_filters * 4, 3, padding=1)
+        self.conv4 = nn.Conv2d(
+            hparams.base_filters * 2, hparams.base_filters * 4, 3, padding=1
+        )
         self.bn4 = nn.BatchNorm2d(hparams.base_filters * 4)
         self.pool2 = nn.MaxPool2d(2)
         self.fc1 = nn.Linear(hparams.base_filters * 4, hparams.n_classes)
-        
+
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(self.bn1(x))
@@ -82,49 +86,59 @@ class AudioNet(pl.LightningModule):
         x = F.adaptive_avg_pool2d(x, (1, 1))
         x = self.fc1(x[:, :, 0, 0])
         return x
-    
+
     def training_step(self, batch, batch_idx):
         # Very simple training loop
         x, y = batch
         y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
-        self.log('train_loss', loss, on_step=True)
+        self.log("train_loss", loss, on_step=True)
         return loss
-    
+
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
         y_hat = torch.argmax(y_hat, dim=1)
         acc = functional.accuracy(y_hat, y)
-        self.log('val_acc', acc, on_epoch=True, prog_bar=True)
+        self.log("val_acc", acc, on_epoch=True, prog_bar=True)
         return acc
-        
+
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.optimizer.lr)
         return optimizer
 
 
-@hydra.main(config_path="configs", config_name="default")
+@hydra.main(config_path="configs", config_name="default", strict=False)
 def train(cfg: DictConfig):
-    
+
     logger.info(OmegaConf.to_yaml(cfg))
+
+    wandb.init(config=hparams_default)
+
+    wandb_config_omega = OmegaConf.create(wandb.config._as_dict())
+    cfg.data.sample_rate = wandb_config_omega.sample_rate
+    cfg.model.base_filters = wandb_config_omega.base_filters
+    cfg.model.optim.lr = wandb_config_omega.lr
 
     # We use folds 1,2,3 for training, 4 for validation, 5 for testing.
     path = Path(get_original_cwd()) / Path(cfg.data.path)
     train_data = ESC50Dataset(path=path, folds=cfg.data.train_folds)
     val_data = ESC50Dataset(path=path, folds=cfg.data.val_folds)
     test_data = ESC50Dataset(path=path, folds=cfg.data.test_folds)
-    
-    train_loader = \
-        torch.utils.data.DataLoader(train_data, cfg.data.batch_size, shuffle=True)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_data, cfg.data.batch_size, shuffle=True
+    )
     val_loader = torch.utils.data.DataLoader(val_data, cfg.data.batch_size)
     test_loader = torch.utils.data.DataLoader(test_data, cfg.data.batch_size)
 
     pl.seed_everything(cfg.seed)
 
+    wandb_logger = pl.loggers.WandbLogger()
+
     audionet = AudioNet(cfg.model)
 
-    trainer = pl.Trainer(**cfg.trainer)
+    trainer = pl.Trainer(**cfg.trainer, logger=wandb_logger)
     trainer.fit(audionet, train_loader, val_loader)
 
 
